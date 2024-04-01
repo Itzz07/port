8742f1c70b8d8ac6314af74cdc97f0078859b6f2
Itzz07 <zimbajoel1@gmail.com>
2024-04-01 10:30:55 +0200

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,45 @@ const Home: React.FC = () => {
   
   return (
     <Layout>
-      <section className="lg:py-16">
+      {/* new code  */}
+      <div className="grid grid-cols-2 items-center justify-center h-screen p-4">
+        <div className="flex flex-col items-center justify-center  p-4">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl text-center font-bold mb-8">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-neutral-200 to-zinc-600">
+              Hello, I&apos;m{" Joel K. Zimba"}
+            </span>
+            <animated.h1
+              style={fade}
+              className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600 text-3xl md:text-4xl lg:text-5xl text-center font-extrabold m-4 "
+            >
+              {texts[textIndex]}
+            </animated.h1>
+          </h1>
+          <div className="space-y-4 md:space-y-0 md:space-x-4">
+            <Link href="/contact" className="bg-purple-500 text-white px-4 py-2 rounded block w-full md:inline-block md:w-auto hover:bg-purple-600">
+                Contact Me
+            </Link>
+            <Link href="/projects" className="bg-indigo-500 text-white px-4 py-2 rounded block w-full md:inline-block md:w-auto hover:bg-indigo-600">
+                Projects
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+
+
+      {/* old code  */}
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div
         >
@@ -62,44 +100,11 @@ const Home: React.FC = () => {
             </Link>
           </div>
         </div>
-        <div
-         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
-            />
-          </div>
-        </div>
+        
       </div>
-    </section>
 
-{/* new code  */}
-
-    <div className="flex flex-col items-center justify-center h-screen p-4">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl text-center font-bold mb-8">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" Joel K. Zimba"}
-            </span>
-            <animated.h1
-        style={fade}
-        className="text-3xl md:text-4xl lg:text-5xl text-center font-extrabold mb-8"
-      >
-        {texts[textIndex]}
-      </animated.h1>
-        </h1>
-        <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Link href="/contact" className="bg-purple-500 text-white px-4 py-2 rounded block w-full md:inline-block md:w-auto hover:bg-purple-600">
-              Contact Me
-          </Link>
-          <Link href="/projects" className="bg-indigo-500 text-white px-4 py-2 rounded block w-full md:inline-block md:w-auto hover:bg-indigo-600">
-              Projects
-          </Link>
-        </div>
-      </div>
+      
+    
     </Layout>
   );
 };
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -5,15 +5,24 @@ import Layout from '../components/Layout';
 
 const Projects: React.FC = () => {
   const [tabIndex, setTabIndex] = useState(0);
+  const [detailsVisibility, setDetailsVisibility] = useState<Record<string, boolean>>({});
 
   const projects = [
-    { title: 'Project 1', category: 'mobile' },
-    { title: 'Project 2', category: 'website' },
-    { title: 'Project 3', category: 'mobile' },
+    { title: 'Project 1', category: 'mobile', details: 'Details about Project 1.' },
+    { title: 'Project 2', category: 'website', details: 'Details about Project 2.' },
+    { title: 'Project 3', category: 'mobile', details: 'Details about Project 3.' },
     // Add more projects with appropriate categories
   ];
 
   const filteredProjects = tabIndex === 0 ? projects : projects.filter((project) => project.category === (tabIndex === 1 ? 'mobile' : 'website'));
+
+  const handleProjectClick = (project: any) => {
+    setDetailsVisibility((prevVisibility) => ({
+      ...prevVisibility,
+      [project.title]: !prevVisibility[project.title],
+    }));
+  };
+
 
   return (
     <Layout>
@@ -79,7 +88,35 @@ const Projects: React.FC = () => {
             ))}
           </div>
         </TabPanel>
+
+        
       </Tabs>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4  justify-around content-center m-12">
+        {filteredProjects.map((project, index) => (
+          <div key={index} className="bg-gray-800 p-4 rounded-lg">
+            <h2 className="text-xl font-bold text-white mb-2">{project.title}</h2>
+            <p className="text-gray-400">Category: {project.category}</p>
+            {detailsVisibility[project.title] ? (
+              <div>
+                <p className="text-gray-400 mb-2">{project.details}</p>
+                {project.category === 'website' ? (
+                  <a href={''} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Visit Website</a>
+                ) : (
+                  <button className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg">
+                    Download APK
+                  </button>
+                )}
+              </div>
+            ) : null}
+            <button
+              onClick={() => handleProjectClick(project)}
+              className="text-purple-500 hover:text-white transition duration-300"
+            >
+              {detailsVisibility[project.title] ? 'Hide Details' : 'View Details'}
+            </button>
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 };
